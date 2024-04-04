822b30e9ecb2a0e8ad2eb98cc6b5181195d9713e
JJamVa <hongi97@naver.com>
2024-04-04 21:39:33 +0900

    feat: reservation 컴포넌트

--- src/components/reservation/Reservation.tsx
+++ src/components/reservation/Reservation.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { MakeReservation } from "./makeReservation/MakeReservation";
 import { ReservationDetails } from "./reservationDetails/ReservationDetails";
 
 function Reservation() {
+
+  const [params, setParams] = useState<any>({
+    helMngerCd: null,
+    useDate: null,
+    useStTime: null,
+    useEdTime: null,
+    userDiv: 1,
+    reqEmpNo: null,
+  });
+
   return (
-    <div>
+    <div style={{display: "flex", flexDirection:"row", gap:"50px"}}>
       <MakeReservation />
       <ReservationDetails />
     </div>
