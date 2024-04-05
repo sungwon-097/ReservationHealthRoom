88e97fbc799b425b9c3195521d8cc1faeafbd907
JJamVa <hongi97@naver.com>
2024-04-05 20:21:17 +0900

    style: 불필요한 파일 삭제 및 코드 주석

--- src/components/PreWork.tsx
+++ src/components/PreWork.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { ContainerItem } from "./common/ContainerItem";
-import { constURL } from "../utils/ConstURL";
-import * as S from "../App.styles";
+// import React from "react";
+// import { ContainerItem } from "./common/ContainerItem";
+// import { constURL } from "../utils/ConstURL";
+// import * as S from "../App.styles";
 
-export const PreWork = () => {
-  return (
-    <ContainerItem
-      title={"1. 사전 작업"}
-      content={
-        <p>
-          <S.A href={constURL.timsUrl}>TALK 로그인</S.A> (10분 전 권장) 및 해당
-          페이지 팝업 허용
-        </p>
-      }
-    />
-  );
-};
+// export const PreWork = () => {
+//   return (
+//     <ContainerItem
+//       title={"1. 사전 작업"}
+//       content={
+//         <p>
+//           <S.A href={constURL.timsUrl}>TALK 로그인</S.A> (10분 전 권장) 및 해당
+//           페이지 팝업 허용
+//         </p>
+//       }
+//     />
+//   );
+// };
