0f3e316a99d705c0bfa0c3195519b13ea032b75d
JJamVa <hongi97@naver.com>
2024-04-05 20:22:36 +0900

    fix: 시간 표현 방식 수정

--- src/utils/ParseDate.ts
+++ src/utils/ParseDate.ts
@@ -1,10 +1,5 @@
-export const parseDate = (date: any) => {
-  return date
-    .toLocaleDateString("ko-KR", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    })
-    .replace(/\. /g, "")
-    .substring(0, 8);
+export const parseDate = (date: Date) => {
+  const offset = date.getTimezoneOffset() * 60000; // 타임존 오프셋을 밀리초로 계산
+  const localDate = new Date(date.getTime() - offset); // 로컬 날짜 조정
+  return localDate.toISOString().split("T")[0]; // "YYYY-MM-DD" 형식으로 변환
 };
