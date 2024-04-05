fffecb703bfbee9f0f7387dcbb56c29780c3ca38
JJamVa <hongi97@naver.com>
2024-04-05 20:18:04 +0900

    style: 스타일링 분리 및 불필요한 스타일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import "moment/locale/ko";
-import { PreWork } from "./components/PreWork";
-import { MakeReservationList } from "./components/MakeReservationList";
-import { Reservation } from "./components/Reservation";
-import { ReservationHistory } from "./components/ReservationHistory";
-import { Header } from "./components/common/Header";
-// import { BoxContainer } from "./components/common/BoxContainer";
-import { TimsLogin } from "./components/timsLogin/TimsLogin";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { EmployeeNumField } from "./components/employeeNumField/EmployeeNumField";
+import { Header } from "./components/common/header/Header";
 import { AppRoutes } from "./routes/AppRoutes";
+import * as S from "./App.styles";
 
 function App() {
   /**
@@ -21,23 +14,10 @@ function App() {
 
   return (
     <>
-      <Header reservationList={reservationList} />
-      <div
-        style={{
-          width: "100%",
-          height: "calc(100vh - 59px)",
-          backgroundColor: "#f5f5f5",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {/* <PreWork />
-      <MakeReservationList setReservationList={setReservationList} />
-      <Reservation reservationList={reservationList} />
-      <ReservationHistory /> */}
+      <Header />
+      <S.BackgroundWrapper>
         <AppRoutes />
-      </div>
+      </S.BackgroundWrapper>
     </>
   );
 }
